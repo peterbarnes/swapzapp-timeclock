c0065f632f6abfae4a828f4cdd995fbe048fc2f5
Peter Barnes <barnespeter213@gmail.com>
2014-04-16 17:03:13 -0500

    Finally make this shit work

--- app/models/timecard.rb
+++ app/models/timecard.rb
@@ -1,19 +1,18 @@
 class Timecard
   include ActiveModel::Model
 
-  attr_accessor :id, :first_name, :last_name
+  attr_accessor "first_name", "last_name"
 
-  validates_presence_of :first_name
+  validates_presence_of "first_name", "last_name"
 
   def self.all
     @timecards = []
     @data = JSON.parse(RestClient.get(self._base_url))
-    @data.each do |_timecard|
-      @timecards << Timecard.new(first_name: _timecard[1])
+    @data['users'].each do |_timecard|
+      @timecards << Timecard.new(first_name: _timecard['first_name'], last_name: _timecard['last_name'])
     end
     @timecards
   end
-
   private
 
     def self._base_url
